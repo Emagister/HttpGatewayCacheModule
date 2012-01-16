ec3abc42a8247d2b273213205b71d5b3438b43fb
Christian <csoronellas@emagister.com>
2012-01-16 16:24:03 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HttpGatewayCacheModule functional test
+This is a functional test for the ```HttpGatewayCacheModule``` based on the [ZF2 tutorial](https://github.com/akrabat/zf2-tutorial) by Rob Alen.
---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\ActionController,
+    Album\Model\AlbumTable,
+    Album\Form\AlbumForm;
+
+class AlbumController extends ActionController
+{
+    /**
+     * @var \Album\Model\AlbumTable
+     */
+    protected $albumTable;
+
+    public function indexAction()
+    {
+        $this->getEvent()->getResponse()->headers()->addHeaderLine('Cache-control: max-age=1800');
+        return array(
+            'albums' => $this->albumTable->fetchAll(),
+        );
+    }
+
+    public function addAction()
+    {
+        $form = new AlbumForm();
+        $form->submit->setLabel('Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $formData = $request->post()->toArray();
+            if ($form->isValid($formData)) {
+                $artist = $form->getValue('artist');
+                $title  = $form->getValue('title');
+                $this->albumTable->addAlbum($artist, $title);
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('default', array(
+                    'controller' => 'album',
+                    'action'     => 'index',
+                ));
+
+            }
+        }
+
+        $this->getEvent()->getResponse()->headers()->addHeaderLine('Cache-control: max-age=1800');
+        return array('form' => $form);
+
+    }
+
+    public function editAction()
+    {
+        $form = new AlbumForm();
+        $form->submit->setLabel('Edit');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $formData = $request->post()->toArray();
+            if ($form->isValid($formData)) {
+                $id     = $form->getValue('id');
+                $artist = $form->getValue('artist');
+                $title  = $form->getValue('title');
+
+                if ($this->albumTable->getAlbum($id)) {
+                    $this->albumTable->updateAlbum($id, $artist, $title);
+                }
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('default', array(
+                    'controller' => 'album',
+                    'action'     => 'index',
+                ));
+            }
+        } else {
+            $id = $request->query()->get('id', 0);
+            if ($id > 0) {
+                $form->populate($this->albumTable->getAlbum($id));
+            }
+        }
+
+        return array('form' => $form);
+    }
+
+    public function deleteAction()
+    {
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $del = $request->post()->get('del', 'No');
+            if ($del == 'Yes') {
+                $id = $request->post()->get('id');
+                $this->albumTable->deleteAlbum($id);
+            }
+
+            // Redirect to list of albums
+            return $this->redirectToList();
+        }
+
+        $id = $request->query()->get('id', 0);
+        return array('album' => $this->albumTable->getAlbum($id));
+    }
+
+    public function esiAction()
+    {
+        $response = new \Zend\Http\PhpEnvironment\Response();
+        $response->headers()->addHeaderLine('Cache-control: max-age=3600');
+        $response->setContent($this->getLocator()->get('view')->render('album/esi.phtml'));
+
+        return $response;
+    }
+
+    public function setAlbums(AlbumTable $albumTable)
+    {
+        $this->albumTable = $albumTable;
+        return $this;
+    }
+}
