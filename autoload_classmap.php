8c17441dccca0704c7722e419ded572748d858f0
Christian Soronellas <theunic@gmail.com>
2012-03-09 01:07:16 +0100

    Added the ZF1 component branch

--- autoload_classmap.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'HttpGatewayCacheModule\\Module'        => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
-  'Emagister\\HttpGatewayCache'           => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Emagister' . DIRECTORY_SEPARATOR . 'HttpGatewayCache.php',
-  'Emagister\\CacheAware'                 => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Emagister' . DIRECTORY_SEPARATOR . 'CacheAware.php',
-  'Emagister\\ProcessorAwareInterface'    => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Emagister' . DIRECTORY_SEPARATOR . 'ProcessorAware.php',
-  'Emagister\\Esi\\Processor'             => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Emagister' . DIRECTORY_SEPARATOR . 'Esi' . DIRECTORY_SEPARATOR . 'Processor.php',
-  'Emagister\\Esi\\View\\Helper\\Esi'     => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Emagister' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Helper' . DIRECTORY_SEPARATOR . 'Esi.php',
-);
--- src/Emagister/View/Helper/Esi.php
+++ src/Emagister/View/Helper/Esi.php
@@ -2,10 +2,26 @@
 
 namespace Emagister\View\Helper;
 
-use Zend\View\Helper\HtmlElement;
+use Zend\View\Helper\AbstractHelper,
+    Zend\View\Helper\Action as ActionHelper;
 
-class Esi extends HtmlElement
+class Esi extends AbstractHelper
 {
+    /**
+     * @var \Zend\View\Helper\Action
+     */
+    private $actionHelper;
+
+    /**
+     * Class constructor
+     *
+     * @param \Zend\View\Helper\Action $actionHelper
+     */
+    public function __construct(ActionHelper $actionHelper)
+    {
+        $this->actionHelper = $actionHelper;
+    }
+
     public function __invoke($uri, $alt = null, $continueOnError = false)
     {
         return '<esi:include src="' . $uri . '"' . (null !== $alt ? ' alt="' . $alt . '"' : '') . (false !== $continueOnError ? ' continue="continue"' : '') . '/>';
